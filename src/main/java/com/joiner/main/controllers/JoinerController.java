a6dc3a9b1ca6151a35cfde7925f568868059ac1f
Jorge Díaz <jorge.diaz@endava.com>
2021-04-06 11:59:38 -0500

    Update joiner flow

--- src/main/java/com/joiner/main/controllers/JoinerController.java
+++ src/main/java/com/joiner/main/controllers/JoinerController.java
@@ -1,9 +1,11 @@
 package com.joiner.main.controllers;
 
 import com.joiner.main.dtos.JoinerDto;
+import com.joiner.main.dtos.UpdateJoinerDto;
 import com.joiner.main.models.Joiner;
 import com.joiner.main.services.CreateJoinerServiceInterface;
 import com.joiner.main.services.ShowJoinerServiceInterface;
+import com.joiner.main.services.UpdateJoinerServiceInterface;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -26,14 +28,16 @@ import javax.validation.Valid;
 public class JoinerController {
     private final CreateJoinerServiceInterface createJoinerServiceInterface;
     private final ShowJoinerServiceInterface showJoinerServiceInterface;
+    private final UpdateJoinerServiceInterface updateJoinerServiceInterface;
 
     @Autowired
     public JoinerController(
             CreateJoinerServiceInterface createJoinerServiceInterface,
-            ShowJoinerServiceInterface showJoinerServiceInterface
-    ) {
+            ShowJoinerServiceInterface showJoinerServiceInterface,
+            UpdateJoinerServiceInterface updateJoinerServiceInterface) {
         this.createJoinerServiceInterface = createJoinerServiceInterface;
         this.showJoinerServiceInterface = showJoinerServiceInterface;
+        this.updateJoinerServiceInterface = updateJoinerServiceInterface;
     }
 
     @PostMapping
@@ -56,5 +60,16 @@ public class JoinerController {
     public Joiner show(@PathVariable Long id) {
         log.info("Search a joiner with ID {}", id);
         return this.showJoinerServiceInterface.execute(id);
+    }
+
+    @PutMapping("/{id}")
+    @ApiOperation(value = "Return joiner updated")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Joiner searched"),
+            @ApiResponse(code = 404, message = "Joiner not found")
+    })
+    public Joiner update(@PathVariable Long id, @Valid @RequestBody UpdateJoinerDto updateJoinerDto) {
+        log.info("Update a joiner with ID {} data {}", id, updateJoinerDto);
+        return this.updateJoinerServiceInterface.execute(id, updateJoinerDto);
     }
 }
