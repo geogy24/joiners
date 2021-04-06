92fdd5647f92e10dba813e2dfb2bd54b67a6f018
Jorge Díaz <jorge.diaz@endava.com>
2021-04-06 08:25:09 -0500

    Service's repositories

---
+++ src/main/java/com/joiner/main/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.joiner.main.repositories;
+
+import com.joiner.main.models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+}
